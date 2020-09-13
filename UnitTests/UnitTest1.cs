9152661f7023f9fc6906244fdf18feef707e64c8
Nor Shiervani <50927017+NorShiervani@users.noreply.github.com>
2020-09-13 12:35:12 +0200

    Seperated projects and remaking pipeline.

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,27 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TestAPI;
+using TestAPI.Controllers;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestIfWeatherForecastsExist()
+        {
+            //Arrange
+            IEnumerable<WeatherForecast> forecasts;
+
+            //Act
+            forecasts = WeatherForecastController.Get();
+
+            //Assert
+            bool forecastsExist = forecasts != null && forecasts.Count() > 0;
+            Assert.IsTrue(forecastsExist, "Weatherforecasts does not exist.");
+        }
+    }
+}
